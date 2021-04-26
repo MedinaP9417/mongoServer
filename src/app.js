27d75f44b1c33e9d9cefb0bd5b4efe6c97004837
Medina Pilavdzic <medina.pilavdzic@gmail.com>
2021-04-26 19:45:13 +0200

    Added cors to package.json

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
+import cors from 'cors';
 import router from './router';
 
 const app = express();
 app.use(bodyParser.json());
 app.use(helmet());
+app.use(cors());
 
 router(app);
 
